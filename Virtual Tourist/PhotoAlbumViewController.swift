40c856c6d6ce17688bd0c2f53084f17c16262963
Mihir Thanekar <mihirthanekar@gmail.com>
2017-07-23 22:49:28 -0700

    Refactor: if let
    Added settings for photo album.

--- Virtual Tourist/PhotoAlbumViewController.swift
+++ Virtual Tourist/PhotoAlbumViewController.swift
@@ -15,14 +15,26 @@ class PhotoAlbumViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        if mapLocation != nil {
+        
+        // Settings
+        smallMapView.isScrollEnabled = false
+        smallMapView.isZoomEnabled = false
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let pointLoc = mapLocation {
             //smallMapView.setCenter(mapLocation!, animated: false)
             var region = MKCoordinateRegion()
-            region.center = mapLocation!
-            region.span = MKCoordinateSpan(latitudeDelta: 10, longitudeDelta: 10)
+            region.center = pointLoc
+            region.span = MKCoordinateSpan(latitudeDelta: 1, longitudeDelta: 1)
             smallMapView.setRegion(region, animated: false)
+            
+            let mapAnnotation = MKPointAnnotation()
+            mapAnnotation.coordinate = pointLoc
+            smallMapView.addAnnotation(mapAnnotation)   // Add our annotation
         }
-    }
 
+    }
 
 }
