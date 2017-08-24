81dc922e9f7e187db8df53e269b1283b9a799cde
Mihir Thanekar <mihirthanekar@gmail.com>
2017-08-23 23:38:08 -0700

    Autosave every 5 seconds.

--- Virtual Tourist/AppDelegate.swift
+++ Virtual Tourist/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         //  try! stack?.dropAllData() // FOR DEBUGGING ONLY
-        stack?.autosave(10)
+        stack?.autosave(5)
         return true
     }
 }
